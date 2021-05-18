34f7d210c253ece470adb37eb85f5208d2fd7f41
Aswin <cgaswin333@gmail.com>
2021-05-18 10:40:09 +0530

    fixed bugs

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,7 @@
 import './Home.css';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Navbar from '../../Components/Navbar/Navbar';
 import Book from '../../Assets/images/BookLanding.jpg'
