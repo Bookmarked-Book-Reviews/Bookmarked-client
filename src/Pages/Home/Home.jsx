f0cb9894076928389e8009bf44b2c036410e4e41
Aswin <cgaswin333@gmail.com>
2021-05-18 10:42:47 +0530

    fixed button not use d error

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import './Home.css';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
