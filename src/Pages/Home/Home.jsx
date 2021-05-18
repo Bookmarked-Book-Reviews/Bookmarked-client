efe32de009b94cc51d2d078cdbff5fbdbc7e2755
Aswin <cgaswin333@gmail.com>
2021-05-18 10:36:38 +0530

    creating home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,37 @@
 import './Home.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Navbar from '../../Components/Navbar/Navbar';
+import Book from '../../Assets/images/BookLanding.jpg'
+
 
 function Home() {
   return (
-    <>
-    </>
+    <div style={{backgroundColor:'white'}}>
+    <CssBaseline/>
+     <Navbar/>
+     <Container>
+     <Container maxWidth="sm">
+      <Typography variant='h3'align='left'color='textPrimary'>
+       THE BEST WAY TO DISCOVER AND SHARE NEW BOOKS
+      </Typography>
+      <Typography variant='subtitile1'color='textSecondary'>
+       Bookmarked is an online social book review and recommendation platform where you can find all the information you want from various categories such as new releases or best sellers. You can support your favorite author by giving your rating or writing a short review.
+      </Typography>
+     </Container>
+     <figure>
+     <img src={Book} alt="Books"width='50vh' height='50vh'/>
+     </figure>
+     </Container>
+
+
+    </div>
   );
 }
 
