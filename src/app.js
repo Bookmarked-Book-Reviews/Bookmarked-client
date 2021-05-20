c852316885d1d4f131456e16cb8996e912306c62
Aswin <cgaswin333@gmail.com>
2021-05-20 11:56:56 +0530

    moved styling of navbar to separate file

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import {Route,Switch} from 'react-router-dom';
 import Home from './Pages/Home/Home.jsx';
 import Login from './Pages/Login/Login.jsx';
 import Signup from './Pages/Signup/Signup.jsx';
-import About from './Pages/About/About.jsx';
 import Error from './Pages/Error/Error.jsx'
 import Dashboard from './Pages/Dashboard/Dashboard.jsx';
 import AddBook from './Pages/AddBook/AddBook.jsx';
@@ -16,7 +15,6 @@ export default function App() {
                 <Route path="/" component={Home} exact />
                 <Route path="/login" component={Login} />
                 <Route path="/signup" component={Signup} />
-                <Route path="/about" component={About} />
                 <Route path="/dashboard" component={Dashboard} />
                 <Route path="/create" component={AddBook} />
                 <Route component={Error} />
