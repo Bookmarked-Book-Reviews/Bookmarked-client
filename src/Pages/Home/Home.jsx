6af1dd142fc75014e75878652e29ad1f26d9e9b1
Aswin <cgaswin333@gmail.com>
2021-05-21 12:16:45 +0530

    removed text decoration for links in dashboard

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -14,14 +14,12 @@ function Home() {
      <Container width="100%">
         <Grid container spacing={3} justify="flex-start"  direction="row">
           <Grid item sm={6} >
-            <Container maxWidth="sm">
                 <Typography variant='h3'align='left'color='textPrimary'>
                 THE BEST WAY TO DISCOVER AND SHARE NEW BOOKS
                 </Typography>
                 <Typography variant='subtitile1'color='textSecondary'>
                 Bookmarked is an online social book review and recommendation platform where you can find all the information you want from various categories such as new releases or best sellers. You can support your favorite author by giving your rating or writing a short review.
                 </Typography>
-            </Container>
             </Grid>
 
             <Grid item sm={6}>
