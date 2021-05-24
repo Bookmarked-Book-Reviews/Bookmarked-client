a274fcd3828a512525177145d991923677101142
Aswin <cgaswin333@gmail.com>
2021-05-24 19:00:31 +0530

    added axios

--- src/Components/BookCard/BookCard.jsx
+++ src/Components/BookCard/BookCard.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-import PersonIcon from '@material-ui/icons/Person';
 import Button from '@material-ui/core/Button';
 import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
 import RateReviewIcon from '@material-ui/icons/RateReview';
