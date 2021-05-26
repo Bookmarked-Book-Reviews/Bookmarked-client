f30df2ce2ef112e1a39467b6f78721c161c91c61
Aswin <cgaswin333@gmail.com>
2021-05-26 13:44:30 +0530

    toggle login logout

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import {Link} from 'react-router-dom';
 import useStyles from './NavbarStyling';
+import {UserContext} from "../../app";
 
 
 export default function Navbar() {
   const classes = useStyles();
+  const {state,} = useContext(UserContext);
 
-  return (
-    <div className={classes.root}>
+  const RenderMenu=()=>{
+
+   if (state){
+     return(
+      <>
+        <div className={classes.root}>
+      <AppBar position="static" style={{ backgroundColor:'transparent',boxShadow:'none',color:'black',}}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Bookmarked
+          </Typography>
+        <Link to="/" style={{textDecoration:"none" ,color:"black"}}>  <Button color="inherit">Home</Button></Link>
+        <Link to="/signup" style={{textDecoration:"none" ,color:"black"}}> <Button color="inherit">SignUp</Button></Link>
+        <Link to="/logout" style={{textDecoration:"none" ,color:"black"}}><Button color="inherit">Logout</Button></Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+      </>
+     )
+    
+   }
+   else{
+     return(
+       <>
+       <div className={classes.root}>
       <AppBar position="static" style={{ backgroundColor:'transparent',boxShadow:'none',color:'black',}}>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
@@ -20,9 +45,18 @@ export default function Navbar() {
         <Link to="/" style={{textDecoration:"none" ,color:"black"}}>  <Button color="inherit">Home</Button></Link>
         <Link to="/signup" style={{textDecoration:"none" ,color:"black"}}> <Button color="inherit">SignUp</Button></Link>
         <Link to="/login" style={{textDecoration:"none" ,color:"black"}}><Button color="inherit">Login</Button></Link>
-        <Link to="/logout" style={{textDecoration:"none" ,color:"black"}}><Button color="inherit">Logout</Button></Link>
         </Toolbar>
       </AppBar>
     </div>
+       </>
+     )
+   }
+
+  }
+
+  return (
+    <>
+     <RenderMenu/>
+    </>
   );
 }
