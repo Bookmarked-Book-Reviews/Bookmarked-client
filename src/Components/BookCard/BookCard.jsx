7490e5f0cbf958f99d761770b46ad09f18be1051
Aswin <cgaswin333@gmail.com>
2021-05-26 15:51:20 +0530

    fixing issues

--- src/Components/BookCard/BookCard.jsx
+++ src/Components/BookCard/BookCard.jsx
@@ -29,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
 export default function BookCard(props) {
   const classes = useStyles();
   const history = useHistory();
-  book=props.data;
+  const book=props.data;
 
   return(
     
